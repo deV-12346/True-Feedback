70769d3cea09755363e81c97255a0db2ae55f34f
Dev Raj <dr395108@gmail.com>
2025-09-27 16:57:51 +0530

    added check username availbility

---
+++ src/app/api/user/check-username-unique/route.ts
@@ -0,0 +1,48 @@
+import { connectDB } from "@/lib/dbConnect";
+import { UserModel } from "@/model/User.Model";
+import { usernameValidation } from "@/schemas/signupSchema";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+const usernameQuerySchema = z.object({
+      username:usernameValidation
+})
+export async function GET(req:Request){
+      await connectDB()
+      try {
+           const {searchParams} = new URL(req.url)
+           const queryParams = {
+            username : searchParams.get("username")
+           } 
+           const result = usernameQuerySchema.safeParse(queryParams)
+
+           if(!result.success){
+            const usernameError = result.error.format().username?._errors || []
+            console.log(usernameError)
+               return NextResponse.json({
+                  success:false,
+                  message:usernameError
+               },{status:400})
+           }
+           const {username} = result.data
+           const findUsername = await UserModel.findOne({
+                username:username,isVerified:true
+           })
+           if(findUsername){
+            return NextResponse.json({
+                  success:false,
+                  message:"Username already taken"
+            },{status:400})
+           } 
+           return NextResponse.json({
+                  success:true,
+                  message:"Username is available"
+           },{status:200})
+      } catch (error) {
+           console.log(error)
+           return NextResponse.json({
+            success:false,
+            message:"Error chekcing Username"
+           },{status:500}) 
+      }
+}
