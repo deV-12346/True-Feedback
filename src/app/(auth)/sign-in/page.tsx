9d0d187b918b3630de8a1e6ee859a01b50bcb1c8
Dev Raj <dr395108@gmail.com>
2025-09-27 15:29:42 +0530

    Added auth provider

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -14,7 +14,7 @@ export default function Component() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button className="bg-indigo-400 px-10 py-4 rounded"  onClick={() => signIn()}>Sign in</button>
     </>
   )
 }
