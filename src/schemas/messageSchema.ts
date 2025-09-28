668f5945b2968f9841e29ecf42b8ebcff13a5836
Dev Raj <dr395108@gmail.com>
2025-09-28 17:28:31 +0530

    updated messageschema

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,6 +1,6 @@
 import {z} from "zod"
-const messagesSchea = z.object({
-      context:z.string()
-      .min(10,{message:"Message must be of  atleast 4 characters"})
+export const messagesSchema = z.object({
+      content:z.string()
+      .min(10,{message:"Message must be of atleast 10 characters"})
       .max(100,{message:"Message must be no more than 100 characters"})
 })
