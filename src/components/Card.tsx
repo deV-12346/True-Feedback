c592831be90c638847d8105c05b47921bc496abf
Dev Raj <dr395108@gmail.com>
2025-10-02 23:50:34 +0530

    added message card

---
+++ src/components/Card.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React from 'react'
+import {
+      Card,
+      CardAction,
+      CardContent,
+      CardDescription,
+      CardFooter,
+      CardHeader,
+      CardTitle,
+} from "@/components/ui/card"
+import { Message } from '@/model/User.Model'
+import axios from "axios"
+import { toast } from 'sonner'
+type messageCardProps = {
+      message: Message,
+      handleDeleteMessage:(messageID:string)=>void
+}
+const MessageCard = ({message,handleDeleteMessage}:messageCardProps) => {
+      const onMessageDeleteConfirm = async() =>{
+            const res = axios.delete(`/api/message/delete-message/${message._id}`)
+            toast.success(res.data?.message || "Message deleted")
+            handleDeleteMessage(message._id)
+      }
+      return (
+            <div>
+            <Card>
+                  <CardHeader>
+                  {/* <CardTitle>Card Title</CardTitle> */}
+                  <CardDescription>{message.content}</CardDescription>
+                  <CardAction onClick={onMessageDeleteConfirm}></CardAction>
+                  </CardHeader>
+                  {/* <CardContent>
+                  <p>{message.content}</p>
+                  </CardContent> */}
+                  <CardFooter>
+                  <p>{new Date (message.createdAt)}</p>
+                  </CardFooter>
+            </Card>
+            </div>
+      )
+}
+
+export default MessageCard
